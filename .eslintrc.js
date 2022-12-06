ac3ddd989a136f463330e181716c1268a9cf3c38
mooniiDev <mooniidev@proton.me>
2022-12-06 20:26:17 +0200

    Update configuration of ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,24 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:prettier/recommended",
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
   ],
+  overrides: [],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react', 'react-hooks'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-uses-react": 0,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
 };
-Footer;
