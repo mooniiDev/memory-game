b4bdc609a90fcba331ba804d8ad8c20954c88559
mooniiDev <mooniidev@proton.me>
2023-01-11 19:40:37 +0200

    Create a mock of Card and map it into CardsList

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -1,5 +1,19 @@
-function Card() {
-  return <div>Plant Card</div>;
+// Packages imports
+import { string } from 'prop-types';
+
+function Card(props) {
+  const { cardNumber } = props;
+  return (
+    <button type="button" className="w-1/6">
+      Plant{cardNumber}
+      &nbsp;
+    </button>
+  );
 }
+
+// Validating prop types
+Card.propTypes = {
+  cardNumber: string.isRequired,
+};
 
 export default Card;
