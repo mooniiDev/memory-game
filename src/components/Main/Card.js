76629b5f732ceeee4feb1c659144ee956a1d29d3
mooniiDev <mooniidev@proton.me>
2023-02-15 18:14:24 +0200

    Add style for cards on hover, update cards layout and responsive style

--- src/components/Main/Card.js
+++ src/components/Main/Card.js
@@ -5,7 +5,11 @@ function Card(props) {
   const { handleCardClick, cardImage } = props;
 
   return (
-    <button type="button" className="m-3 w-1/6" onClick={handleCardClick}>
+    <button
+      type="button"
+      className="my-3 mx-4 w-1/6 drop-shadow-md saturate-50 transition ease-in-out active:scale-125 active:saturate-100 lg:w-1/6 lg:drop-shadow-xl lg:hover:scale-110 lg:hover:saturate-100"
+      onClick={handleCardClick}
+    >
       <img src={cardImage} alt="Card of Plant" />
     </button>
   );
